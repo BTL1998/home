3e4802c8a378c1d8edc2e16a9e9cf2e6149fe8d4
BTL1998 <thanaphonlikhit@gmail.com>
2025-08-12 22:26:23 +0700

    IconLock-4488b7fa.js

---
+++ assets/IconLock-4488b7fa.js
@@ -0,0 +1,27 @@
+import {_ as o, o as i, c, a as t} from "./index-19f29003.js";
+const r = {}
+  , s = {
+    width: "24",
+    height: "24",
+    viewBox: "0 0 24 24",
+    fill: "none",
+    xmlns: "http://www.w3.org/2000/svg"
+};
+function l(n, e) {
+    return i(),
+    c("svg", s, e[0] || (e[0] = [t("g", {
+        "clip-path": "url(#clip0_1083_769)"
+    }, [t("path", {
+        d: "M5.25 9H6V6C6 2.691 8.691 0 12 0C15.309 0 18 2.691 18 6V9H18.75C19.99 9 21 10.009 21 11.25V21.75C21 22.991 19.99 24 18.75 24H5.25C4.01 24 3 22.991 3 21.75V11.25C3 10.009 4.01 9 5.25 9ZM16 6C16 3.794 14.206 2 12 2C9.794 2 8 3.794 8 6V9H16V6ZM11 16.722V19C11 19.552 11.447 20 12 20C12.553 20 13 19.552 13 19V16.722C13.595 16.375 14 15.737 14 15C14 13.897 13.103 13 12 13C10.897 13 10 13.897 10 15C10 15.737 10.405 16.375 11 16.722Z",
+        fill: "#C5AAFF"
+    })], -1), t("defs", null, [t("clipPath", {
+        id: "clip0_1083_769"
+    }, [t("rect", {
+        width: "24",
+        height: "24",
+        fill: "white",
+        transform: "matrix(-1 0 0 1 24 0)"
+    })])], -1)]))
+}
+const a = o(r, [["render", l]]);
+export {a as I};
